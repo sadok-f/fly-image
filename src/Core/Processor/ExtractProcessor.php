061f595e023847530336085fc4b46e83e9a14417
Marek Trofimiuk <m.trofimiuk@vin-info.pl>
2021-02-24 10:09:18 +0100

    gravity parameter added to extract

--- src/Core/Processor/ExtractProcessor.php
+++ src/Core/Processor/ExtractProcessor.php
@@ -29,11 +29,13 @@ class ExtractProcessor extends Processor
         $topLeftY = $outputImage->extractKey('extract-top-y');
         $bottomRightX = $outputImage->extractKey('extract-bottom-x');
         $bottomRightY = $outputImage->extractKey('extract-bottom-y');
+        $gravity = $outputImage->extractKey('gravity');
 
         $geometryW = $bottomRightX - $topLeftX;
         $geometryH = $bottomRightY - $topLeftY;
         $extractCmd = new Command(self::IM_CONVERT_COMMAND);
         $extractCmd->addArgument($inputImage->sourceImagePath());
+        $extractCmd->addArgument(" -gravity", "$gravity");
         $extractCmd->addArgument(" -crop", "{$geometryW}x{$geometryH}+{$topLeftX}+{$topLeftY}");
         $extractCmd->addArgument($inputImage->sourceImagePath());
         $this->execute($extractCmd);
